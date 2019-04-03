873a6b7e78ec9e2ca57e359958a41a37033debb4
Reema <it.reema7@gmail.com>
2019-04-04 01:05:12 +0300

    Add files via upload

---
+++ MemeMe 1.0/MemeMe 1.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe 1.0
+//
+//  Created by mac_os on 12/03/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topTextField: String
+    var bottomTextField: String
+    var originalImage: UIImage?
+    var memedImage: UIImage?
+}
